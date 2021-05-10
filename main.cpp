2b2890a907634df8cce93e964f3709f1f3b74402
Mateusz Kacperski <oureyelet@gmail.com>
2021-05-10 18:06:44 +0100

    update 4.12 — An introduction to string (std::ws)

--- main.cpp
+++ main.cpp
@@ -52,6 +52,21 @@ int main()
     a chance to enter an age.
     */
 
+    /*-----------------------
+    What the heck is std::ws?
+    -----------------------*/
+    /*
+    Best practice:
+
+    If using std::getline to read strings, use the std::ws input manipulator 
+    to ignore leading whitespace.
+
+    Key insight:
+
+    Using the extraction operator (>>) with std::cin ignores leading whitespace.
+    std::getline does not ignore leading whitespace unless you use input manipulator std::ws.
+    */
+
     /*------------------------------
     Use std::getline() to input text
     ------------------------------*/
